b92566d4b648b0a83bc92c066cd5e7cdd586b4eb
squid <SpaceMonkey1347@gmail.com>
2025-04-23 23:05:26 -0400

    desktop commit: 2025-04-23 23-05-26

--- bot.py
+++ bot.py
@@ -16,10 +16,6 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 assert isinstance(TOKEN, str)
 
-
-user_input = input("Type something!")
-print(user_input)
-
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 help_command = commands.DefaultHelpCommand(show_parameter_descriptions=False)
